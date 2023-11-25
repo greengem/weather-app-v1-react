54ec2bcba2432ad26713635cac1707c9f0d9e356
Green Gem <chris_waitt@me.com>
2023-11-25 10:26:26 +0000

    Added a placeholder button to get current location

--- src/components/WeatherSearch/WeatherSearch.tsx
+++ src/components/WeatherSearch/WeatherSearch.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { IconLocation, IconSearch } from '@tabler/icons-react';
+
 interface WeatherSearchProps {
   onSearch: (searchTerm: string) => void;
 }
@@ -17,14 +19,14 @@ const WeatherSearch: React.FC<WeatherSearchProps> = ({ onSearch }) => {
   };
 
   return (
-    <div>
+    <>
       <label htmlFor="hs-trailing-button-add-on-with-icon" className="sr-only">Search for a city</label>
       <div className="flex rounded-md">
         <input 
           type="text" 
           id="hs-trailing-button-add-on-with-icon" 
           name="hs-trailing-button-add-on-with-icon" 
-          className="py-2 px-4 block w-full border-gray-200 rounded-l-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" 
+          className="py-2 px-4 block rounded-l-md text-xs text-gray-800 w-[120px]" 
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           onKeyDown={handleKeyDown}
@@ -32,15 +34,13 @@ const WeatherSearch: React.FC<WeatherSearchProps> = ({ onSearch }) => {
         />
         <button 
           type="button" 
-          className="inline-flex flex-shrink-0 justify-center items-center h-[2.875rem] w-[2.875rem] rounded-r-md border border-transparent font-semibold bg-pink-500 text-white hover:bg-pink-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-pink-500 transition-all text-sm"
+          className="inline-flex flex-shrink-0 justify-center items-center rounded-r-md border border-transparent bg-purple-500 text-xs px-3"
           onClick={handleSearchClick}
-        >
-          <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-          </svg>
+        ><IconSearch size={16} />
         </button>
       </div>
-    </div>
+      <button className='bg-purple-500 px-3 rounded-md text-xs'><IconLocation size={16} /></button>
+    </>
   );
 }
 
