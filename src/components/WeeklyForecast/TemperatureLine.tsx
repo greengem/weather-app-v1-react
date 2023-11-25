f852aa04d5f03478341fb3e310496d167717e9bb
Green Gem <chris_waitt@me.com>
2023-11-25 00:37:04 +0000

    adjusted abolute poitioning of svg graph

--- src/components/WeeklyForecast/TemperatureLine.tsx
+++ src/components/WeeklyForecast/TemperatureLine.tsx
@@ -5,7 +5,7 @@ interface TemperatureLineProps {
 
 const TemperatureLine: React.FC<TemperatureLineProps> = ({ maxTempPathData, minTempPathData }) => {
   return (
-    <svg style={{ top: "165px" }} className="absolute left-0 w-full" viewBox="0 0 461 150" preserveAspectRatio="none">
+    <svg style={{ top: "155px" }} className="absolute left-0 w-full" viewBox="0 0 461 150" preserveAspectRatio="none">
       <defs>
         <linearGradient id="maxTempGradient" x1="0%" y1="0%" x2="100%" y2="0%">
           <stop offset="0%" stopColor="#f9d728" />
