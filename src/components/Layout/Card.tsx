a32f7aa4d6493d76b6c2891491ac40fb5af1d8db
Green Gem <chris_waitt@me.com>
2023-11-26 10:12:19 +0000

    Card styling and allow any content in title field

--- src/components/Layout/Card.tsx
+++ src/components/Layout/Card.tsx
@@ -1,22 +1,22 @@
 import { ReactNode, FC } from 'react';
 
 interface CardProps {
-  title?: string;
+  title?: ReactNode;
   icon?: ReactNode;
   children: ReactNode;
   className?: string;
 }
 
 const Card: FC<CardProps> = ({ title, icon, children, className }) => {
-  const defaultClasses = 'p-3 shadow-lg text-white rounded-lg bg-gradient-to-br from-gray-800 to-gray-700 opacity-70';
+  const defaultClasses = 'p-3 shadow-lg text-white rounded-lg bg-gradient-to-br from-gray-800 to-gray-700';
   const combinedClasses = `${defaultClasses} ${className || ''}`.trim();
 
   return (
     <div className={combinedClasses}>
       {(icon || title) && (
-        <div className="flex text-xs gap-1 text-blue-200 mb-3 justify-start items-center">
+        <div className="flex text-xs gap-1 text-blue-200 mb-3 justify-start items-center ">
           {icon && <span>{icon}</span>}
-          {title && <span className='uppercase font-semibold'>{title}</span>}
+          {title && <span className='uppercase font-semibold one-line-text'>{title}</span>}
         </div>
       )}
       <div className='text-xs'>
